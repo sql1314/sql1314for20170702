9274007350c4398673d273c737793cea8490ca6c
System Administrator <root@yanghaiyandeMacBook-Pro.local>
2017-07-09 17:21:33 +0800

    用户界面

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\Controller;
 class UserController extends Controller{
     public function showProfile($id)
     {
-        Redis::set('user',"user001");
-        Redis::set('user',"user002");
+        Redis::set('user:profile:1',"user001");
+        Redis::set('user:profile:2',"user002");
         $user = Redis::get('user:profile:'.$id);
         echo 1;
-        var_dump($user);die;
+        var_dump($user);
         return view('user.profile',['user' => $user]);
     }
 }
