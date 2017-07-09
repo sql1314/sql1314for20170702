6684aa69775500f4858f4d8632a8e7b1c7dbd5bf
System Administrator <root@yanghaiyandeMacBook-Pro.local>
2017-07-09 16:58:01 +0800

    模板

---
+++ laravel/resources/views/user/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+    <meta charset="utf-8">
+    {{--通用的js或者css--}}
+    </script>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
